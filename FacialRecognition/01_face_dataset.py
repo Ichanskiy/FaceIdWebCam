306bbbd3aef107458f35d5dad517d394ba13c152
Ichanskiy <ichanskiy.olexandr@gmail.com>
2018-11-28 11:30:54 +0200

    update all

--- FacialRecognition/01_face_dataset.py
+++ FacialRecognition/01_face_dataset.py
@@ -1,3 +1,5 @@
+import re
+
 import cv2
 import os
 import numpy as np
@@ -9,7 +11,8 @@ dirPath = ""
 listFiles = []
 
 for (dirPath, dirnames, filenames) in os.walk(
-        "C:/Users/Ichanskiy/PycharmProjects/FaceIdWebCam/FacialRecognition/input/"):
+        # "C:/Users/Ichanskiy/PycharmProjects/FaceIdWebCam/FacialRecognition/input/"):
+        "D:/Diplom/testing/input/"):
     listFiles = filenames
     print(list)
     print(dirPath)
@@ -21,7 +24,8 @@ face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 listFiles.sort(reverse=True)
 for fileName in listFiles:
     im = Image.open(dirPath + fileName)
-    nameImage = fileName.replace(".jpg", "")
+    idImage = fileName.replace(".jpg", "")
+    idImage = re.findall("\d+", idImage)[0]
     im2arr = np.array(im)
 
     img = cv2.flip(im2arr, 1)  # flip video image vertically
@@ -32,7 +36,8 @@ for fileName in listFiles:
         for (x, y, w, h) in faces:
             cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
             # Save the captured image into the datasets folder
-            cv2.imwrite("C:/Users/Ichanskiy/PycharmProjects/FaceIdWebCam/FacialRecognition/dataset/" + str(
-                id) + '.' + str(i) + ".jpg", gray[y:y + h, x:x + w])
+            # cv2.imwrite("C:/Users/Ichanskiy/PycharmProjects/FaceIdWebCam/FacialRecognition/dataset/" + str(
+            cv2.imwrite("D:/Diplom/testing/dataset/" + str(
+                idImage) + '.' + str(i) + ".jpg", gray[y:y + h, x:x + w])
             # count += 1
     id = id + 1
--- FacialRecognition/03_face_recognition.py
+++ FacialRecognition/03_face_recognition.py
@@ -1,77 +1,93 @@
-''''
-Real Time Face Recogition
-	==> Each face stored on dataset/ dir, should have a unique numeric integer ID as 1, 2, 3, etc                       
-	==> LBPH computed model (trained faces) should be on trainer/ dir
-Based on original code by Anirban Kar: https://github.com/thecodacus/Face-Recognition    
-
-Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18  
-
-'''
-
+import datetime
+import sys
 import cv2
 import numpy as np
-import os 
+import os
+from PIL import Image
+import mysql.connector
+
+userIdFromConsole = sys.argv[1]
+pathInputImage = sys.argv[2]
+# userIdFromConsole = 117
+# pathInputImage = "D:/Diplom/testing/recognition/117.dsf.jpg"
+
+print(userIdFromConsole)
+print(pathInputImage)
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
-recognizer.read('C:/Users/Ichanskiy/PycharmProjects/OpenCV-Face-Recognition/FaceDetection/trainer/trainer.yml')
-cascadePath = "haarcascade_frontalface_default.xml"
-faceCascade = cv2.CascadeClassifier(cascadePath);
+recognizer.read('C:/Users/Ichanskiy/PycharmProjects/FaceIdWebCam/FacialRecognition/trainer/trainer.yml')
+cascadePath = "C:/Users/Ichanskiy/PycharmProjects/FaceIdWebCam/FacialRecognition/Cascades/haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-#iniciate id counter
-id = 0
+photoIds = set()
+predictedUserID = 0
 
-# names related to ids: example ==> Marcelo: id=1,  etc
-names = ['None', 'Ichanskiy', 'Сергій Вікторович', 'Ilza', 'Z', 'W']
-
-# Initialize and start realtime video capture
-cam = cv2.VideoCapture(0)
-cam.set(3, 640) # set video widht
-cam.set(4, 480) # set video height
-
-# Define min window size to be recognized as a face
-minW = 0.1*cam.get(3)
-minH = 0.1*cam.get(4)
-
-while True:
-
-    ret, img =cam.read()
-    img =cv2.flip(img, 1) # Flip vertically
-
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-    faces = faceCascade.detectMultiScale( 
+for i in range(1):
+    im = Image.open(pathInputImage)
+    im2arr = np.array(im)
+    img = cv2.flip(im2arr, 1)  # flip video image vertically
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
         gray,
-        scaleFactor = 1.2,
-        minNeighbors = 5,
-        minSize = (int(minW), int(minH)),
-       )
+        scaleFactor=1.2,
+        minNeighbors=5
+    )
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        predictedUserID, confidence = recognizer.predict(gray[y:y + h, x:x + w])
+        # Check if confidence is less them 100 ==> "0" is perfect match
+        if (confidence < 60):
+            print("predicted id = ", predictedUserID)
+            photoIds.add(predictedUserID)
 
-    for(x,y,w,h) in faces:
-
-        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
-
-        id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
-
-        # Check if confidence is less them 100 ==> "0" is perfect match 
-        if (confidence < 100):
-            id = names[id]
             confidence = "  {0}%".format(round(100 - confidence))
         else:
-            id = "unknown"
+            predictedUserID = "unknown"
             confidence = "  {0}%".format(round(100 - confidence))
-        
-        cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
-        cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
-    
-    cv2.imshow('camera',img) 
-
-    k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
-    if k == 27:
-        break
+        # cv2.putText(img, str(id), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+        # cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
+    cv2.imshow('camera', img)
 
 # Do a bit of cleanup
 print("\n [INFO] Exiting Program and cleanup stuff")
-cam.release()
-cv2.destroyAllWindows()
+# print(studentsIds)
+
+cnx = mysql.connector.connect(user='root', password='1111',
+                              host='localhost',
+                              database='attendance')
+cursor = cnx.cursor()
+
+now = datetime.datetime.now()
+
+if (len(photoIds) == 0 or len(photoIds) >= 2):
+    sql = "INSERT INTO attendance (date, presence, user_id) VALUES (%s, %s, %s)"
+    val = (now, False, userIdFromConsole)
+
+    cursor.execute(sql, val)
+    cnx.commit()
+
+for photoId in photoIds:
+    # select_stmt = "SELECT id FROM students WHERE id = %(emp_no)s"
+    # cursor.execute(select_stmt, { 'emp_no': photoId })
+    # myresult = cursor.fetchall()
+    # userId = 0
+    # for x in myresult:
+    #     userId = x[0]
+    sql = "INSERT INTO attendance (date, presence, user_id) VALUES (%s, %s, %s)"
+    print("---")
+    userIdFromConsole = int(userIdFromConsole)
+    predictedUserID = int(predictedUserID)
+    print(userIdFromConsole)
+    print(predictedUserID)
+    print("---")
+    if (userIdFromConsole == predictedUserID):
+        val = (now, True, userIdFromConsole)
+    else:
+        val = (now, False, userIdFromConsole)
+
+    cursor.execute(sql, val)
+    cnx.commit()
+
+    print(cursor.rowcount, "record inserted.")
